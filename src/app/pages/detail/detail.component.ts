1a95f948a4e51817a049b7c7cd4fe8efc1353b6b
janynnegomes <janynnegomes@gmail.com>
2020-06-01 00:26:47 -0300

    build for Github Pages hosting

--- src/app/pages/detail/detail.component.ts
+++ src/app/pages/detail/detail.component.ts
@@ -10,7 +10,7 @@ import { ActivatedRoute, Router } from '@angular/router';
 export class DetailComponent implements OnInit {
 
   categoryLink:string = ""
-  category = {}
+  category:any = {}
   categories = []
   total = 0
 
